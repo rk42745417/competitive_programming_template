4a9014a0c24dff8b01ebcfc86603bdb6045769da
rk42745417 <rk427454171@gmail.com>
2022-01-27 19:06:34 +0800

    update

--- default.cpp
+++ default.cpp
@@ -27,11 +27,11 @@ using ll = int64_t;
 using ull = uint64_t;
 using ld = long double;
 using uint = uint32_t;
-const double EPS  = 1e-7;
+const double EPS  = 1e-8;
 const int INF     = 0x3F3F3F3F;
 const ll LINF     = 4611686018427387903;
 const int MOD     = 1e9+7;
-static auto Lamy_is_cute = []() {
+static int Lamy_is_cute = []() {
 	EmiliaMyWife
 	return 48763;
 }();
