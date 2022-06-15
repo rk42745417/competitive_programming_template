ec21bb98065188ea4ff5098b0e1fe3e7faf6234d
Ji Kuai <rk427454171@gmail.com>
2022-06-15 20:13:56 +0800

    debug macro

--- default.cpp
+++ default.cpp
@@ -11,8 +11,23 @@
 */
 #include <bits/stdc++.h>
 using namespace std;
+
+#ifdef EMT
 #define debug(x) cerr << "\e[1;31m" << #x << " = " << (x) << "\e[0m\n"
 #define print(x) emilia_mata_tenshi(#x, begin(x), end(x))
+template<typename T, typename T2> ostream& operator<<(ostream &os, const pair<T, T2> &obj) {
+	return os << '{' << obj.first << ',' << obj.second << '}';
+}
+template<class TupType, size_t... I> void lamy_kawaii(ostream& os, const TupType& _tup, index_sequence<I...>) {
+	// source: https://stackoverflow.com/a/41171552
+	os << '{';
+	(..., (cerr << (I == 0? "" : ",") << get<I>(_tup)));
+	os << '}';
+}
+template<class... T> ostream& operator<<(ostream &os, const tuple<T...>& _tup) {
+	lamy_kawaii(os, _tup, make_index_sequence<sizeof...(T)>());
+	return os;
+}
 template<typename T> void emilia_mata_tenshi(const char *s, T l, T r) {
 	cerr << "\e[1;33m" << s << " = [";
 	while(l != r) {
@@ -21,7 +36,13 @@ template<typename T> void emilia_mata_tenshi(const char *s, T l, T r) {
 	}
 	cerr << "\e[0m\n";
 }
+#else
+#define debug(x) 48763
+#define print(x) 48763
+#endif
 
+#define YN(x) ((x) ? "YES" : "NO")
+#define yn(x) ((x) ? "Yes" : "No")
 #define EmiliaMyWife ios::sync_with_stdio(0); cin.tie(NULL);
 using ll = int64_t;
 using ull = uint64_t;
---
+++ dijkstra.cpp
@@ -0,0 +1,5 @@
+template<T>
+class shortest_path {
+	vector<T> dis;
+};
+/****************** Dijkstra's algorithm *****************/
